300372193a64d3450b10ea00b550c5e6bdfaafa5
David Lewis <davidlewisp@gmail.com>
2018-01-12 11:17:31 -0800

    Stuck on jquerey to display track from if statement. The correct value is being captured but it will not show

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,14 @@ $(document).ready(function() {
   });
 
   $("#survey").submit(function() {
-  
+    // $(".tracks").hide();
+    var design = $("#designQuestion").val();
+
+    if(design === "Design") {
+      $(".tracks#css").show();
+    } else {
+      alert("try again");
+    }
 
 
     event.preventDefault();
