af9312aac8c9a0fa6735308e81a1dcc779073cc2
David Lewis <davidlewisp@gmail.com>
2018-01-12 10:25:34 -0800

    Add sumbit button and test submit functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@
 $(document).ready(function() {
   $("#showAll").click(function() {
     $(".tracks").toggle();
+  });
+
+  $("#survey").submit(function() {
+  
+
 
     event.preventDefault();
   });
